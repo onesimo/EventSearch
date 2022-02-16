15d36bde2d06851822515f061d0834db97a356db
Onésimo Batista <onesimobatista@gmail.com>
2022-02-16 12:22:45 +0000

    Fix email validation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:6|confirmed'
         ];
     }
